e5ef643fd15974a1527ab749846eb84d38b6178f
StellarisLD <leandrogridelli@gmail.com>
2023-05-05 16:08:13 +0200

    Cicli, bug fix

---
+++ C - Cicli/cicli2.py
@@ -0,0 +1,6 @@
+# Leandro Gridelli
+
+import random
+
+for k in range(7,2,-1):
+    print(f"\nCiclo eseguito {k} volte")
---
+++ test.py
@@ -0,0 +1,26 @@
+a=20
+
+b=5*2
+
+c=50
+
+
+
+if a>b:
+
+    if b>c:
+
+        print('Mare')
+
+    else:
+
+        print('Sole')
+
+elif a<b:
+
+    print("Luna")
+
+else:
+
+    print("Pioggia")
+
