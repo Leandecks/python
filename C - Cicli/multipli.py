cbd18c1c81ee894e3ac1e21f85e3ae14eb8966bd
StellarisLD <leandrogridelli@gmail.com>
2023-05-08 17:27:14 +0200

    Multipli con cicli

---
+++ C - Cicli/multipli.py
@@ -0,0 +1,15 @@
+# Leandro Gridelli
+
+'''
+Il computer riceve 6 nr da tastiera
+Quanti sono multipli di 5?
+'''
+
+contatore = 0
+n=1
+
+while True:
+    n = int(input("Inserisci un numero: "))
+    contatore+=1
+    if n==0:
+        break
