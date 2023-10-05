71989d5d852558b912a398d429d0ba7558dd2bf2
Leandecks <leandrogridelli@gmail.com>
2023-10-05 20:02:46 +0200

    Update liste9.py

--- 2/Liste/liste9.py
+++ 2/Liste/liste9.py
@@ -4,9 +4,14 @@ a =  [1,2,4,6,7]
 b = [3,2,1,6,3]
 risultato = 0
 
-for i,k in enumerate(a):
-   if k == b[i]:
-      print(f"Elemento corrispondente {k} in posizione {i}")
-      risultato += 1
+# for i,k in enumerate(a):
+#    if k == b[i]:
+#       print(f"Elemento corrispondente {k} in posizione {i}")
+#       risultato += 1
+
+for k in range(len(a)):
+  if a[k] == b[k]:
+    print(f"Elemento corrispondente {a[k]} in posizione {k}")
+    risultato += 1
 
 print(f"Risposta: {risultato}")
