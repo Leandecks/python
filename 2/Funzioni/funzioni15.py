6c15ea877c70b33bac64871d148a39f9bef6c452
Leandecks <leandrogridelli@gmail.com>
2024-03-22 22:21:58 +0100

    Functions 15 part 2

--- 2/Funzioni/funzioni15.py
+++ 2/Funzioni/funzioni15.py
@@ -2,12 +2,23 @@
 
 def dispari(lista, n):
     somma_dispari = 0
+
     for k in lista:
         if k % 2 != 0:
             somma_dispari += k
 
     return somma_dispari * n
 
+def posizioni(lista, n):
+    somma = 0
+
+    for k in range(len(lista)):
+        if k % 2 != 0:
+            somma += lista[k] // n
+
+    return somma
+
 # main
 
 print(dispari([2,3,4,5],3))
+print(posizioni([2,6,3,15,4], 3))
