81e2a39bcf0c07c8a9b816b062d9a75dab1005b3
Leandecks <leandrogridelli@gmail.com>
2024-04-05 16:28:21 +0200

    Functions 16-18

---
+++ 2/Funzioni/T_funzioni16.py
@@ -0,0 +1,7 @@
+# Leandro Gridelli
+
+def cubo(l):
+    return l ** 3
+
+def quadrato(l):
+    return l ** 2
---
+++ 2/Funzioni/T_funzioni17.py
@@ -0,0 +1,9 @@
+# Leandro Gridelli
+
+# from T_funzioni16 import cubo
+# from T_funzioni16 import * NON FARE
+import T_funzioni16
+
+a = int(input("Inserisci numero: "))
+
+print(T_funzioni16.cubo(a))
