33256aa7a86ba508c46d2655a041c9ae3ebadb6e
Leandecks <leandrogridelli@gmail.com>
2024-04-11 14:34:39 +0200

    Docstring for Functions 21

--- 2/Funzioni/funzioni21.py
+++ 2/Funzioni/funzioni21.py
@@ -5,6 +5,18 @@ import math
 
 def is_triangolo(triangolo):
 
+    """
+    (list) -> Optional[float, bool]
+
+    Restituisce l'area del triangolo se il parametro fornito è un triangolo.
+    Se no restituisce False.
+
+    >>> is_triangolo([3, 4, 5])
+    6.0
+    >>> is_triangolo([100, 1, 1])
+    False
+    """
+
     a, b, c = sorted(triangolo)
     p = (a + b + c) / 2
 
@@ -16,4 +28,10 @@ def is_triangolo(triangolo):
 
 # main
 
-print(is_triangolo([4, 3, 5]))
+lati = []
+
+for k in range(3):
+    lati.append(int(input("Inserisci un lato: ")))
+
+
+print(is_triangolo(lati))
