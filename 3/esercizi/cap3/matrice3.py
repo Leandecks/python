e08522470432c8da54acd5c24692fab592fbbab3
Leandecks <leandrogridelli@gmail.com>
2025-02-18 21:24:51 +0100

    Exercise 3 on matrixes

--- 3/esercizi/cap3/matrice3.py
+++ 3/esercizi/cap3/matrice3.py
@@ -12,7 +12,7 @@ def genera_matrice():
         r = []
         numero = random.randint(1, 99)
         r.append(numero)
-        for j in range(colonne - 1):
+        for j in range(colonne):
             punti = random.randint(0, 30)
             r.append(punti)
                 
@@ -26,10 +26,71 @@ def print_matrice(m):
             print(i, end = "\t")
         print()
 
+def trova_moda(lista):
+    d = {}
+
+    for el in lista:
+        if el in d:
+            d[el] += 1
+        else:
+            d[el] = 1
+
+    mass = 0
+
+    for k in d:
+        if d[k] > mass:
+            mass = d[k]
+            massimo = k
+
+    if list(d.values()).count(mass) > 1:
+        return -1
+
+    return massimo
+
+def trova_mediana(lista):
+    lista = sorted(lista)
+
+    if len(lista) % 2 == 0:
+        return (lista[len(lista) // 2] + lista[(len(lista) // 2) - 1]) / 2
+    else:
+        return lista[len(lista) // 2]
+
+def genera_matrice2(mat):
+    righe = len(mat)
+    colonne = 4
+
+    m = []
+
+    for riga in mat:
+        r = []
+        r.append(riga[0])
+
+        c = 0
+        lista_punti = []
+
+        for colonna in range(1, len(riga)):
+            lista_punti.append(riga[colonna])
+
+        media = round(sum(lista_punti) / len(lista_punti), 2)
+        moda = trova_moda(lista_punti)
+        mediana = trova_mediana(lista_punti)
+
+        r.append(media)
+        r.append(moda)
+        r.append(mediana)
+
+        m.append(r)
+
+    return m
+
 # main
 
 matrice = genera_matrice()
-print(matrice)
+print()
 print_matrice(matrice)
+print()
 
-# nr, media
+# nr, media, mediana, moda
+
+matrice2 = genera_matrice2(matrice)
+print_matrice(matrice2)
